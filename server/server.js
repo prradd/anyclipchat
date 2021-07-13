4413f3372f4c7f4a73a96d5b9943cc3a3c1572f8
Anton Shestakov <prradd@gmail.com>
2021-07-13 18:55:28 +0300

    most of the UI done

--- server/server.js
+++ server/server.js
@@ -6,19 +6,42 @@ const io = require('socket.io')(http, {
         methods: ["GET"]
     }
 });
-const path = require('path');
+
+const formatMessage = require('./utils/messages');
+const {userJoin, getCurrentUser, userLeave, getChatUsers} = require("./utils/users");
+
+const botName = 'AnyClip Bot';
 
 io.on('connection', socket => {
+    socket.on('joinChat', username => {
+        const user = userJoin(socket.id, username)
 
-    // Welcome current user
-    socket.emit('message', 'Welcome to AnyClip chat!')
+        // Welcome current user
+        socket.emit('message', formatMessage(botName,'Welcome to AnyClip chat!'))
 
-    // Broadcast when user connects
-    socket.broadcast.emit('message', 'A user has joined the chat');
+        // Broadcast when user connects
+        socket.broadcast.emit('message', formatMessage(botName,`${username} has joined the chat`));
+
+        // Send all users
+        io.emit('chatUsers', getChatUsers());
+    })
+
+    // Listen for chatMessage
+    socket.on('chatMessage', (msg) => {
+        const user = getCurrentUser(socket.id);
+        io.emit('message', formatMessage(user?.username, msg));
+    })
 
     // When user disconnects
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the chat')
+        const user = userLeave(socket.id);
+
+        if (user) {
+            io.emit('message', formatMessage(botName,`${user.username} has left the chat`))
+
+            // Send all users
+            io.emit('chatUsers', getChatUsers());
+        }
     })
 })
 
